57e245ef7af8c7cd47d4970fd5702ac0392469fc
Daniel Schneider <daschne8@gmail.com>
2019-02-21 17:36:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  phrase = ""
+  if num % 3 == 0
+    phrase += "Fizz"
+  elsif num % 5 == 0
+    phrase += "Buzz"
+  elsif phrase == ""
+    return nil
+  end
+  return phrase
+end
